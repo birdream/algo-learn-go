e6de04593f2663b012c88d4fa9b86a9d96785d94
birdream <bird.dream.norman@gmail.com>
2019-08-09 17:51:57 +0800

    stash for not solving quick 2 problem

--- Sort/quick-sort-2.go
+++ Sort/quick-sort-2.go
@@ -1,28 +1,38 @@
 package main
 
 func quickSort2(sortArr []int, left, right int) {
+
 	if left < right {
-		key := sortArr[(left+right)/2]
+		p := (left + right) / 2
+		key := sortArr[p]
 		i, j := left, right
 
 		for {
+			// fmt.Println("111")
+			in := false
 			for sortArr[i] < key {
+				// fmt.Println("111111111111")
+				in = true
 				i++
 			}
-
+			// fmt.Println("222")
 			for sortArr[j] > key {
+				// fmt.Println("222222222222")
+				in = true
 				j--
 			}
-
-			if i >= j {
+			// fmt.Println("333")
+			// fmt.Println(i)
+			// fmt.Println(j)
+			if i >= j || !in {
 				break
 			}
-
+			// fmt.Println("444")
 			sortArr[i], sortArr[j] = sortArr[j], sortArr[i]
 		}
 
-		quickSort2(sortArr, left, i-1)
-		quickSort2(sortArr, j+1, right)
+		quickSort2(sortArr, left, p-1)
+		quickSort2(sortArr, p+1, right)
 	}
 }
 
