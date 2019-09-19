63fab476d6b172c32cfa7ae1109cf1d6ff022037
birdream <bird.dream.norman@gmail.com>
2019-09-19 16:02:39 +0800

    sort colors 3 way

--- sort012.go
+++ sort012.go
@@ -24,6 +24,11 @@ func sortColors(arr []int) []int {
 }
 
 // 3路快排
+// 数组分为3个区间 0在最前面 1在中间 2在最后面
+// [0..zero, 1,...., two, 2, ...]
+// 如果遍历到arr[i]为1则直接跳过
+// 如果是2 则和arr[two]交换 i不需要++ 因为此时的 arr[two]还为遍历所以不知数值是多少 继续循环
+// 如果是0 则和arr[zero+1]交换 因为arr[zero+1]一定是1 所以无需再判断 直接i++ 继续循环
 func sortColorsBy3Way(arr []int) []int {
 	zero := -1      // arr[0...zero]
 	two := len(arr) // arr[two... len-1]
